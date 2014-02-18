69a0a4ee12e7d570f97b8152285a0a459ca2929a
Andrey Babushkin <andrey_babushkin@epam.com>
2014-02-18 21:21:54 +0400

    '2.0.0218.47;'

--- www/js/i.js
+++ www/js/i.js
@@ -1,7 +1,11 @@
 ﻿window.Test = {};
 
 $(function() {
-	DevExpress.devices.current({platform: 'android'});
+	// DevExpress.devices.current({platform: 'generic'});
+	DevExpress.devices.current({platform: 'andrid'});
+	// DevExpress.devices.current({platform: 'ios', version: '6'});
+	// DevExpress.devices.current({platform: 'win8'});
+
 	Test.app = new DevExpress.framework.html.HtmlApplication({
 		namespace: Test,
 		navigationType: "slideout",
@@ -20,7 +24,10 @@ Test.Clients = function (params) {
 
 	var viewModel = {
 		dataSource: 
-		[{"Id":"-1","N":"Beverages"},{"Id":"-2","N":"Condiments"},{"Id":"-3","N":"Confections"},{"Id":"-4","N":"Dairy Products"},{"Id":"-5","N":"Grains/Cereals"},{"Id":"-6","N":"Meat/Poultry"},{"Id":"-7","N":"Produce"},{"Id":"-8","N":"Seafood"}]
+		[
+		{"Id":"-1","N":"Beverages"},{"Id":"-2","N":"Condiments"},{"Id":"-3","N":"Confections"},{"Id":"-4","N":"Dairy Products"},{"Id":"-5","N":"Grains/Cereals"},{"Id":"-6","N":"Meat/Poultry"},{"Id":"-7","N":"Produce"},{"Id":"-8","N":"Seafood"}
+		,{"Id":"-1","N":"Beverages"},{"Id":"-2","N":"Condiments"},{"Id":"-3","N":"Confections"},{"Id":"-4","N":"Dairy Products"},{"Id":"-5","N":"Grains/Cereals"},{"Id":"-6","N":"Meat/Poultry"},{"Id":"-7","N":"Produce"},{"Id":"-8","N":"Seafood"}
+		]
 	};
 
 	return viewModel;
